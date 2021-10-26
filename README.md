1dcaf4fb6777c4e1949a4d2b84d42c3f832d64e2
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2021-10-26 20:29:50 +0200

    Add Docker workflow

--- README.md
+++ README.md
@@ -1,43 +1,50 @@
 # Cloudflare DDNS
+
 A Python script for dynamically updating the IP address in Cloudflare records. If you have a dynamic public IP address, this script will make the record always point to you. It uses [icanhazip.com](https://github.com/major/icanhaz) to retrieve your public IP address and the Cloudflare API to edit records.
 
 ## Features
- - Supports both IPv4 and IPv6 by default (A and AAAA records)
- - Cleverly updates records (only when needed)
- - Simple console logging
- - Built-in timer
- - Configurable
- - Morphing (create and remove records based on availbable IPs)
- - Overall reserved and reliable approach to editing records
+
+- Supports both IPv4 and IPv6 by default (A and AAAA records)
+- Cleverly updates records (only when needed)
+- Simple console logging
+- Built-in timer
+- Configurable
+- Morphing (create and remove records based on availbable IPs)
+- Overall reserved and reliable approach to editing records
 
 ## Usage
+
 This script has dependencies which can be installed with `pip install -r requirements.txt`. Then running it is simply done as follows.
+
 ```
 python cloudflare.py <path-to-config-file>
 ```
+
 The configuration file is in YAML format and should follow the format in the example config file in this repo `example-config.yml`. Explanations for each of the settings is given there.
 
 ## Automation
+
 There are several ways of automatically running this script to keep records up-to-date in the background. Below are examples of some of these.
 
 ### Docker
-There is a Dockerfile present to build a lean Docker image of this script. This can be done with the following command.
-```
-docker build --tag cloudflare-ddns https://github.com/ldobbelsteen/cloudflare-ddns.git
-```
-Here is an example of running the image.
+
+There is a Dockerfile present to build a lean Docker image of this script. There is a pre-built image available on the GitHub Packages of this repository. An example of how the image can be used:
+
 ```
 docker run \
     --detach \
     --network host \
     --restart always \
     --volume /path/to/config:/config/cloudflare.yml \
-    cloudflare-ddns
+    ghcr.io/ldobbelsteen/cloudflare-ddns:main
 ```
+
 Setting network access to `host` is needed for IPv6 functionality.
 
 ### Systemd
+
 Running the script in systemd is as simple as running any Python script in systemd. Create the file `/etc/systemd/system/cloudflare-ddns.service`
+
 ```
 [Unit]
 Description=Cloudflare dynamic DNS script
@@ -49,16 +56,11 @@ ExecStart=/usr/bin/python -u /path/to/cloudflare.py /path/to/config.yml
 [Install]
 WantedBy=multi-user.target
 ```
+
 Make sure to enable the timer in the config when using systemd. To enable the service, run the following commands:
+
 ```
 systemctl daemon-reload
 systemctl start cloudflare-ddns
 systemctl enable cloudflare-ddns
-```
-
-### Cron
-Using cron is not necessary as the script has a timer mode built in, but it's possible. Make sure to disable the timer in the config file. An example crontab config would look like this.
-```
-# Run the script every 20 minutes
-*/20 * * * * /usr/bin/python -u /path/to/cloudflare.py /path/to/config.yml
 ```
--- cloudflare.py
+++ cloudflare.py
@@ -1,80 +1,123 @@
-import requests
-import socket
 import time
 import json
-import yaml
 import sys
+import socket
+import yaml
+import requests
 
-api_url = "https://api.cloudflare.com/client/v4"
+API_URL = "https://api.cloudflare.com/client/v4"
+
 
 def verify_token(token):
-    header = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + token
-    }
-    url = api_url + "/user/tokens/verify"
-    response = requests.request("GET", url, headers=header)
-    if response.status_code != 200:
-        print("ERROR: Token is invalid")
-        exit()
-    else:
+    try:
+        header = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + token
+        }
+        url = API_URL + "/user/tokens/verify"
+        response = requests.request("GET", url, headers=header)
+        if response.status_code != 200:
+            raise Exception("invalid token")
         return header
+    except:
+        print("ERROR: Token is invalid")
+        sys.exit()
+
 
 def get_zone_id(zone, token):
     try:
-        url = api_url + "/zones?name=" + zone
+        url = API_URL + "/zones?name=" + zone
         response = requests.request("GET", url, headers=token)
         return json.loads(response.text)["result"][0]["id"]
     except:
         print("ERROR: Zone could not be found")
-        exit()
+        sys.exit()
+
 
 def get_records(zone, name, token):
-    url = api_url + "/zones/" + zone + "/dns_records?type=A,AAAA&name=" + name
-    response = requests.request("GET", url, headers=token)
-    records = json.loads(response.text)["result"]
-    a_record = next((record for record in records if record["type"] == "A"), None)
-    aaaa_record = next((record for record in records if record["type"] == "AAAA"), None)
-    return a_record, aaaa_record
+    try:
+        url = API_URL + "/zones/" + zone + "/dns_records?type=A,AAAA&name=" + name
+        response = requests.request("GET", url, headers=token)
+        records = json.loads(response.text)["result"]
+        a_record = next(
+            (record for record in records if record["type"] == "A"), None)
+        aaaa_record = next(
+            (record for record in records if record["type"] == "AAAA"), None)
+        return a_record, aaaa_record
+    except:
+        print("ERROR: Failed to fetch records")
+        sys.exit()
+
 
 def get_ip(version):
     try:
         if version == 4:
-            ipv4 = requests.request("GET", "http://ipv4.icanhazip.com").text.rstrip()
+            ipv4 = requests.request(
+                "GET", "http://ipv4.icanhazip.com").text.rstrip()
             socket.inet_pton(socket.AF_INET, ipv4)
             return ipv4
         if version == 6:
-            ipv6 = requests.request("GET", "http://ipv6.icanhazip.com").text.rstrip()
+            ipv6 = requests.request(
+                "GET", "http://ipv6.icanhazip.com").text.rstrip()
             socket.inet_pton(socket.AF_INET6, ipv6)
             return ipv6
     except:
         return None
 
-def update_record(record, ip, token):
-    url = api_url + "/zones/" + record["zone_id"] + "/dns_records/" + record["id"]
-    response = requests.request("PATCH", url, headers=token, json={
-        "content": ip
-    })
-    result = json.loads(response.text)["result"]
-    print("{} record IP updated from {} to {}...".format(record["type"], record["content"], result["content"]))
-    return result
+
+def update_record(record, address, token):
+    try:
+        url = API_URL + "/zones/" + \
+            record["zone_id"] + "/dns_records/" + record["id"]
+        response = requests.request("PATCH", url, headers=token, json={
+            "content": address
+        })
+        result = json.loads(response.text)["result"]
+        record_type = record["type"]
+        record_content = record["content"]
+        result_content = result["content"]
+        print(
+            f"{record_type} record IP updated from {record_content} to {result_content}...")
+        return result
+    except:
+        print("ERROR: Failed to update record")
+        sys.exit()
+
 
 def create_record(zone, properties, token):
-    url = api_url + "/zones/" + zone + "/dns_records"
-    response = requests.request("POST", url, headers=token, json=properties)
-    result = json.loads(response.text)["result"]
-    print("{} record created with IP {}, a TTL of {} second(s) and proxying {}...".format(result["type"], result["content"], result["ttl"], "on" if result["proxied"] else "off"))
-    return result
+    try:
+        url = API_URL + "/zones/" + zone + "/dns_records"
+        response = requests.request(
+            "POST", url, headers=token, json=properties)
+        result = json.loads(response.text)["result"]
+        result_type = result["type"]
+        result_content = result["content"]
+        result_ttl = result["ttl"]
+        result_proxied = "on" if result["proxied"] else "off"
+        print(f"{result_type} record created with IP {result_content}, a TTL of {result_ttl} second(s) and proxying {result_proxied}...")
+        return result
+    except:
+        print("ERROR: Failed to create record")
+        sys.exit()
+
 
 def remove_record(record, token):
-    url = api_url + "/zones/" + record["zone_id"] + "/dns_records/" + record["id"]
-    response = requests.request("DELETE", url, headers=token)
-    result = json.loads(response.text)["result"]
-    print("{} record has been removed...".format(record["type"]))
-    return result
+    try:
+        url = API_URL + "/zones/" + \
+            record["zone_id"] + "/dns_records/" + record["id"]
+        response = requests.request("DELETE", url, headers=token)
+        result = json.loads(response.text)["result"]
+        record_type = record["type"]
+        print(f"{record_type} record has been removed...")
+        return result
+    except:
+        print("ERROR: Failed to remove record")
+        sys.exit()
+
 
 def update_routine(config):
-    a_record, aaaa_record = get_records(config["zone"], config["record"], config["token"])
+    a_record, aaaa_record = get_records(
+        config["zone"], config["record"], config["token"])
     if a_record is not None or config["morphing"]:
         if not config["disable_ipv4"]:
             ipv4 = get_ip(4)
@@ -104,21 +147,21 @@ def update_routine(config):
                 "proxied": True if a_record is None else a_record["proxied"],
                 "ttl": 1 if a_record is None else a_record["ttl"]
             }
-            aaaa_record = create_record(config["zone"], details, config["token"])
+            aaaa_record = create_record(
+                config["zone"], details, config["token"])
         elif aaaa_record is not None and ipv6 is None and config["morphing"]:
             aaaa_record = remove_record(aaaa_record, config["token"])
 
-def main():
-    config = yaml.safe_load(open(sys.argv[1]))
-    config["record"] = config["zone"] if config["record"] == "@" else config["record"] + "." + config["zone"]
-    config['token'] = verify_token(config['token'])
-    config['zone'] = get_zone_id(config['zone'], config['token'])
-    if config["timer"] == 0:
-        update_routine(config)
-    else:
-        while True:
-            update_routine(config)
-            time.sleep(config['timer'])
 
 if __name__ == "__main__":
-    main()
+    conf = yaml.safe_load(open(sys.argv[1], encoding="UTF-8"))
+    conf["record"] = conf["zone"] if conf["record"] == "@" else conf["record"] + \
+        "." + conf["zone"]
+    conf['token'] = verify_token(conf['token'])
+    conf['zone'] = get_zone_id(conf['zone'], conf['token'])
+    if conf["timer"] == 0:
+        update_routine(conf)
+    else:
+        while True:
+            update_routine(conf)
+            time.sleep(conf['timer'])
